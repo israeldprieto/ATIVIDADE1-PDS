fc68bb9e74438c8312468467e8692ba87f7a2008
israeldprieto <135737445+israeldprieto@users.noreply.github.com>
2024-02-20 16:51:07 -0400

    PRIMEIRO COMMIT

---
+++ Atividade01-Aula-João/CadastroEmpresa.Designer.cs
@@ -0,0 +1,501 @@
+﻿namespace Atividade01_Aula_João
+{
+    partial class CadastroEmpresa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            tx_fantasia = new TextBox();
+            tx_razsocial = new TextBox();
+            tx_capsocial = new TextBox();
+            tx_endereco = new TextBox();
+            tx_nomeprop = new TextBox();
+            tx_cpfprop = new TextBox();
+            cb_situacao = new ComboBox();
+            mb_cnpj = new MaskedTextBox();
+            radioButton1 = new RadioButton();
+            tx_dtinicio = new MaskedTextBox();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            tx_tel = new MaskedTextBox();
+            tx_end = new ComboBox();
+            panel1 = new Panel();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton8 = new RadioButton();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Malgun Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(74, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(229, 30);
+            label1.TabIndex = 1;
+            label1.Text = "CADASTRO EMPRESA";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nome fantasia";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(46, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Razão social";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(46, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 15);
+            label4.TabIndex = 4;
+            label4.Text = "CNPJ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(46, 216);
+            label5.Name = "label5";
+            label5.Size = new Size(102, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Situação cadastral";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(46, 263);
+            label6.Name = "label6";
+            label6.Size = new Size(99, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Regime tributário";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(46, 305);
+            label7.Name = "label7";
+            label7.Size = new Size(146, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Data de início de atividade";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(46, 342);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Telefone";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(46, 383);
+            label9.Name = "label9";
+            label9.Size = new Size(77, 15);
+            label9.TabIndex = 9;
+            label9.Text = "Capital social";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(46, 426);
+            label10.Name = "label10";
+            label10.Size = new Size(110, 15);
+            label10.TabIndex = 10;
+            label10.Text = "Endereço completo";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(46, 463);
+            label11.Name = "label11";
+            label11.Size = new Size(30, 15);
+            label11.TabIndex = 11;
+            label11.Text = "Tipo";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(46, 500);
+            label12.Name = "label12";
+            label12.Size = new Size(99, 15);
+            label12.TabIndex = 12;
+            label12.Text = "Porte da empresa";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(46, 543);
+            label13.Name = "label13";
+            label13.Size = new Size(96, 15);
+            label13.TabIndex = 13;
+            label13.Text = "Natureza jurídica";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(46, 586);
+            label14.Name = "label14";
+            label14.Size = new Size(122, 15);
+            label14.TabIndex = 14;
+            label14.Text = "Nome do proprietário";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(46, 624);
+            label15.Name = "label15";
+            label15.Size = new Size(110, 15);
+            label15.TabIndex = 15;
+            label15.Text = "CPF do proprietário";
+            // 
+            // tx_fantasia
+            // 
+            tx_fantasia.Location = new Point(163, 85);
+            tx_fantasia.Name = "tx_fantasia";
+            tx_fantasia.Size = new Size(100, 23);
+            tx_fantasia.TabIndex = 16;
+            tx_fantasia.TextChanged += tx_fantasia_TextChanged;
+            // 
+            // tx_razsocial
+            // 
+            tx_razsocial.Location = new Point(178, 128);
+            tx_razsocial.Name = "tx_razsocial";
+            tx_razsocial.Size = new Size(100, 23);
+            tx_razsocial.TabIndex = 17;
+            // 
+            // tx_capsocial
+            // 
+            tx_capsocial.Location = new Point(153, 383);
+            tx_capsocial.Name = "tx_capsocial";
+            tx_capsocial.Size = new Size(100, 23);
+            tx_capsocial.TabIndex = 23;
+            tx_capsocial.TextChanged += textBox8_TextChanged;
+            // 
+            // tx_endereco
+            // 
+            tx_endereco.Location = new Point(178, 426);
+            tx_endereco.Name = "tx_endereco";
+            tx_endereco.Size = new Size(100, 23);
+            tx_endereco.TabIndex = 24;
+            // 
+            // tx_nomeprop
+            // 
+            tx_nomeprop.Location = new Point(178, 583);
+            tx_nomeprop.Name = "tx_nomeprop";
+            tx_nomeprop.Size = new Size(100, 23);
+            tx_nomeprop.TabIndex = 28;
+            // 
+            // tx_cpfprop
+            // 
+            tx_cpfprop.Location = new Point(178, 621);
+            tx_cpfprop.Name = "tx_cpfprop";
+            tx_cpfprop.Size = new Size(100, 23);
+            tx_cpfprop.TabIndex = 29;
+            // 
+            // cb_situacao
+            // 
+            cb_situacao.FormattingEnabled = true;
+            cb_situacao.Items.AddRange(new object[] { "Ativa", "Nula", "Suspensa", "Inapta", "Baixada" });
+            cb_situacao.Location = new Point(163, 213);
+            cb_situacao.Name = "cb_situacao";
+            cb_situacao.Size = new Size(121, 23);
+            cb_situacao.TabIndex = 30;
+            // 
+            // mb_cnpj
+            // 
+            mb_cnpj.Location = new Point(126, 172);
+            mb_cnpj.Mask = "00.000.000/0000-00";
+            mb_cnpj.Name = "mb_cnpj";
+            mb_cnpj.Size = new Size(100, 23);
+            mb_cnpj.TabIndex = 33;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(0, 10);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(116, 19);
+            radioButton1.TabIndex = 34;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Simples Nacional";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // tx_dtinicio
+            // 
+            tx_dtinicio.Location = new Point(198, 302);
+            tx_dtinicio.Mask = "00/00/0000";
+            tx_dtinicio.Name = "tx_dtinicio";
+            tx_dtinicio.Size = new Size(100, 23);
+            tx_dtinicio.TabIndex = 35;
+            tx_dtinicio.ValidatingType = typeof(DateTime);
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(122, 10);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(115, 19);
+            radioButton2.TabIndex = 36;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Lucro presumido";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(254, 10);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(47, 19);
+            radioButton3.TabIndex = 37;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Real\r\n";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // tx_tel
+            // 
+            tx_tel.Location = new Point(104, 339);
+            tx_tel.Mask = "(00) 00000-9999";
+            tx_tel.Name = "tx_tel";
+            tx_tel.Size = new Size(100, 23);
+            tx_tel.TabIndex = 38;
+            // 
+            // tx_end
+            // 
+            tx_end.FormattingEnabled = true;
+            tx_end.Items.AddRange(new object[] { "Acre (AC)", "Alagoas (AL)", "Amapá (AP)", "Amazonas (AM)", "Bahia (BA)", "Ceará (CE)", "Distrito Federal (DF)", "Espírito Santo (ES)", "Goiás (GO)", "Maranhão (MA)", "Mato Grosso (MT)", "Mato Grosso do Sul (MS)", "Minas Gerais (MG)", "Pará (PA)", "Paraíba (PB)", "Paraná (PR)", "Pernambuco (PE)", "Piauí (PI)", "Rio de Janeiro (RJ)", "Rio Grande do Norte (RN)", "Rio Grande do Sul (RS)", "Rondônia (RO)", "Roraima (RR)", "Santa Catarina (SC)", "São Paulo (SP)", "Sergipe (SE)", "Tocantins (TO)" });
+            tx_end.Location = new Point(301, 426);
+            tx_end.Name = "tx_end";
+            tx_end.Size = new Size(121, 23);
+            tx_end.TabIndex = 39;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(radioButton1);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton3);
+            panel1.Location = new Point(178, 247);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(334, 34);
+            panel1.TabIndex = 40;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(178, 498);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(72, 19);
+            radioButton4.TabIndex = 41;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Pequeno";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(268, 498);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(59, 19);
+            radioButton5.TabIndex = 42;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Médio";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(343, 496);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(63, 19);
+            radioButton6.TabIndex = 43;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "Grande";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(126, 462);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(58, 19);
+            radioButton7.TabIndex = 44;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "Matriz";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(201, 462);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(49, 19);
+            radioButton8.TabIndex = 45;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "Filial";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "EI- Empresário Individual", "MEI-Microempreendedor Individual", "Sociedade Anônima" });
+            comboBox1.Location = new Point(173, 535);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 46;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(404, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 47;
+            button1.Text = "CADASTRAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // CadastroEmpresa
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(524, 702);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(radioButton8);
+            Controls.Add(radioButton7);
+            Controls.Add(radioButton6);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(panel1);
+            Controls.Add(tx_end);
+            Controls.Add(tx_tel);
+            Controls.Add(tx_dtinicio);
+            Controls.Add(mb_cnpj);
+            Controls.Add(cb_situacao);
+            Controls.Add(tx_cpfprop);
+            Controls.Add(tx_nomeprop);
+            Controls.Add(tx_endereco);
+            Controls.Add(tx_capsocial);
+            Controls.Add(tx_razsocial);
+            Controls.Add(tx_fantasia);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CadastroEmpresa";
+            Text = "CadastroEmpresa";
+            Load += CadastroEmpresa_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private TextBox tx_fantasia;
+        private TextBox tx_razsocial;
+        private TextBox tx_capsocial;
+        private TextBox tx_endereco;
+        private TextBox tx_nomeprop;
+        private TextBox tx_cpfprop;
+        private ComboBox cb_situacao;
+        private MaskedTextBox mb_cnpj;
+        private RadioButton radioButton1;
+        private MaskedTextBox tx_dtinicio;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private MaskedTextBox tx_tel;
+        private ComboBox tx_end;
+        private Panel panel1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private RadioButton radioButton6;
+        private RadioButton radioButton7;
+        private RadioButton radioButton8;
+        private ComboBox comboBox1;
+        private Button button1;
+    }
+}
---
+++ Atividade01-Aula-João/CadastroEmpresa.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade01_Aula_João
+{
+    public partial class CadastroEmpresa : Form
+    {
+        public CadastroEmpresa()
+        {
+            InitializeComponent();
+        }
+
+        private void CadastroEmpresa_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tx_fantasia_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fantasia = (tx_fantasia.Text);
+            string razsocial = (tx_razsocial.Text);
+            string cnpj = (mb_cnpj.Text);
+            string sitcad = (cb_situacao.Text);
+            string regime = ("");
+            string datainicio = (tx_dtinicio.Text);
+            string telefone = (tx_tel.Text);
+            string capital = (tx_capsocial.Text);
+            string endereco = (tx_endereco.Text);
+            string tipo = ("");
+            string porte = ("");
+            string natjuridica = (comboBox1.Text);
+            string nome = (tx_nomeprop.Text);
+            string cpf = (tx_cpfprop.Text);
+
+            if (radioButton1.Checked == true)
+            {
+                regime = radioButton1.Text;
+            }
+            if (radioButton2.Checked == true)
+            {
+                sitcad = radioButton2.Text;
+            }
+            if (radioButton3.Checked == true)
+            {
+                porte = radioButton3.Text;
+            }
+
+            string txt = @"C:\Users\2022102020064\Desktop\AULA01\AULA01.txt";
+            using StreamWriter sw = new StreamWriter(txt);
+            {
+                sw.WriteLine(MessageBox.Show($"CNPJ = {cnpj}\n" + $"Razão Social:{razsocial}\n" + $"Nome fantasia:{fantasia}\n" + $"Situação:{sitcad}\n" +
+                $"Regime Tributário{regime}\n" + $"Data de início:{datainicio}\n" + $"Telefone:{telefone}\n" + $"Capital:{capital}\n" +
+                $"Endereço:{endereco}\n" + $"Tipo:{tipo}\n" + $"Porte da Empresa:{porte}\n" + $"Jurídica:{natjuridica}\n" +
+                $"Proprietário:{nome}\n" + $"Cpf Proprietário:{cpf}\n"));
+
+            }
+
+        }
+    }
+}
